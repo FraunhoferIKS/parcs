262e5a19dce492d43d078eb378283b8060630de7
Alireza Zamanian <alireza.zamanian@iks.fraunhofer.de>
2022-02-24 01:40:17 +0100

    packaging the project, ready to be installed

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rad_sim',
+    version='0.0.1',
+    description='Simulation package',
+    author='RAD-IKS@FhG',
+    author_email='alireza.zamanian@iks.fraunhofer.de',
+    license='BSD 2-clause',
+    packages=find_packages(),
+    install_requires=['numpy~=1.22.2', 'dtaidistance~=2.3.6'],
+    classifiers=[
+        'Development Status :: First Internal Release',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
