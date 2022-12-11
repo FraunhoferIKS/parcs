805457c58ddf57cb07964d449188e29f05b6ce95
Alireza Zamanian <alireza.zamanian@iks.fraunhofer.de>
2022-12-11 22:49:47 +0100

    clean directories

--- main.py
+++
@@ -1,9 +0,0 @@
-from parcs.cdag.graph_objects import Graph
-from parcs.graph_builder.parsers import graph_file_parser
-import numpy
-numpy.random.seed(2022)
-
-nodes, edges = graph_file_parser('graph_description.yml')
-g = Graph(nodes=nodes, edges=edges)
-samples = g.sample(size=5)
-print(samples)
